b2447f6225ec812d47458d33809f1905277c9426
feeloor <felix@khoi.se>
2019-09-14 17:18:16 -0400

    conditionally adding error document config

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,7 +42,11 @@ az login --service-principal --username ${AZURE_CLIENT_ID} --password ${AZURE_SE
 az account set --subscription ${AZURE_SUBSCRIPTION_ID}
 
 # Enable Static Website
-az storage blob service-properties update --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --static-website --404-document ${AZURE_ERROR_DOCUMENT_NAME} --index-document ${AZURE_INDEX_DOCUMENT_NAME}
+if [ -z "$AZURE_ERROR_DOCUMENT_NAME" ]; then
+    az storage blob service-properties update --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --static-website --404-document ${AZURE_ERROR_DOCUMENT_NAME} --index-document ${AZURE_INDEX_DOCUMENT_NAME}
+else
+    az storage blob service-properties update --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --static-website --index-document ${AZURE_INDEX_DOCUMENT_NAME}
+fi
 
 # Upload source to storage
 az storage blob upload-batch -s ${SOURCE_DIR} -d \$web --account-name ${AZURE_STORAGE_ACCOUNT_NAME}
